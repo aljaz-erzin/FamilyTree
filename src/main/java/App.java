a9b4f75fc10d4d42ceb16d363896857366f39d4b
Aljaz Erzin <aerzin@Aljaz.Home>
2018-08-10 14:17:18 +0200

    Setting the environment

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void begin()
+    {
+        System.out.println("Testing environment");
+    }
+    public static void main(String[] args)
+    {
+        begin();
+    }
+}
